cd297796c71d801e01817cc1246a49b54a1d2b18
WeiChienTang <cses3333@gmail.com>
2025-07-18 10:01:59 +0800

    refactor: 簡化多個頁面的組件結構，移除不必要的標籤 feat: 新增按鈕變體和尺寸枚舉，改善按鈕樣式管理

--- ERPCore2/Components/Shared/Buttons/ButtonVariant.cs
+++ ERPCore2/Components/Shared/Buttons/ButtonVariant.cs
@@ -65,25 +65,4 @@ namespace ERPCore2.Components.Shared.Buttons
         /// </summary>
         OutlineInfo
     }
-
-    /// <summary>
-    /// 按鈕尺寸枚舉
-    /// </summary>
-    public enum ButtonSize
-    {
-        /// <summary>
-        /// 小尺寸按鈕
-        /// </summary>
-        Small,
-        
-        /// <summary>
-        /// 正常尺寸按鈕
-        /// </summary>
-        Normal,
-        
-        /// <summary>
-        /// 大尺寸按鈕
-        /// </summary>
-        Large
-    }
 }
