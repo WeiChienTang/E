651a7fdb2d7ba0c1ada0f2e78120f0dd4573ff47
WeiChienTang <cses3333@gmail.com>
2025-08-01 13:33:12 +0800

    Remove OrderStatus and PurchaseType columns from PurchaseOrders table in UpdatePurchaseOrderModel migration

---
+++ ERPCore2/Migrations/20250801052640_UpdatePurchaseOrderModel.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ERPCore2.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdatePurchaseOrderModel : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_PurchaseOrders_OrderStatus_OrderDate",
+                table: "PurchaseOrders");
+
+            migrationBuilder.DropColumn(
+                name: "OrderStatus",
+                table: "PurchaseOrders");
+
+            migrationBuilder.DropColumn(
+                name: "PurchaseType",
+                table: "PurchaseOrders");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "OrderStatus",
+                table: "PurchaseOrders",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<int>(
+                name: "PurchaseType",
+                table: "PurchaseOrders",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_PurchaseOrders_OrderStatus_OrderDate",
+                table: "PurchaseOrders",
+                columns: new[] { "OrderStatus", "OrderDate" });
+        }
+    }
+}
